3c26b3b8f3ab1320bf46d6fc9d6b469ba9c88a92
Alberto Almagro <alberto.almagro@rakuten.com>
2017-03-03 20:39:09 +0100

    Add alias to copy RSA key

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias mv="mv -iv"
 alias ls="ls -FGh"
 alias du="du -cksh"
 alias df="df -h"
+alias rsa_copy="cat ~/.ssh/id_rsa.pub | pbcopy"
 
 # Use modern regexps for sed, i.e. "(one|two)", not "\(one\|two\)".
 alias sed="sed -E"
