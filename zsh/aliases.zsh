d2614da0ec0131a2fa5d3a5e29f2e907d1111d89
Alberto Almagro <alberto.almagro@rakuten.com>
2017-03-04 20:28:18 +0100

    Add Rails aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,7 +8,16 @@ alias mv="mv -iv"
 alias ls="ls -FGh"
 alias du="du -cksh"
 alias df="df -h"
+
+# RSA key pbcopy
 alias rsa_copy="cat ~/.ssh/id_rsa.pub | pbcopy"
+
+# Rails
+alias bers="bundle exec rails server"
+alias bert="bundle exec rails test"
+alias berc="bundle exec rails console"
+alias bercs="bundle exec rails console --sandbox"
+alias ber="bundle exec rspec"
 
 # Use modern regexps for sed, i.e. "(one|two)", not "\(one\|two\)".
 alias sed="sed -E"
